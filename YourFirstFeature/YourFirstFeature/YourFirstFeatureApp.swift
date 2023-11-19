5999a04bc6a7644d200cdc0ff885d34470884a3f
hayabusabusa <hayabusabusaSY@gmail.com>
2023-11-19 21:00:21 +0900

    chore: プロジェクトを追加

---
+++ YourFirstFeature/YourFirstFeature/YourFirstFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFirstFeatureApp.swift
+//  YourFirstFeature
+//
+//  Created by Shunya Yamada on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct YourFirstFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
