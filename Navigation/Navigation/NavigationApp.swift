5da5b2834a1462c91672e90293164998e48573d8
hayabusabusa <hayabusabusaSY@gmail.com>
2023-11-23 19:26:17 +0900

    chore: `Navigation` の章用のプロジェクトを追加

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Shunya Yamada on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
